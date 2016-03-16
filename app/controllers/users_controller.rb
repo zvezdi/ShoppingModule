9aeea668515836a7ce0526b78c44f249c4542523
niko <njichev@gmail.com>
2016-03-16 19:06:56 +0200

    Create users controller and model.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :log_in
+    else
+      render :new
+    end
+  end
+
+  def show
+    render :log_in
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+      .permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
