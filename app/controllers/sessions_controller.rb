08b35d758a106b3bb6d3cfdb3f06d28b89f7dbdd
niko <njichev@gmail.com>
2016-03-16 19:07:39 +0200

    Create sessions controller and helper to manage the cookies.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user&&@user.authenticate(params[:session][:password])
+      log_in @user
+      remember(@user) if params[:remember_me]
+      render 'users/log_in'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to '/users/new'
+  end
+end
