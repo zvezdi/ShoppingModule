e417641f22afe39bc0eacb1ff5deac1a70f4640b
niko <njichev@gmail.com>
2016-03-16 19:08:28 +0200

    Add schema and db migrations for users.

---
+++ db/migrate/20160316165604_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string 'first_name'
+      t.string 'last_name'
+      t.string 'email'
+      t.string 'address'
+      t.string 'phone_address'
+      t.string 'password_digest'
+      t.boolean 'admin', default: false
+
+      t.timestamps null: false
+    end
+  end
+end
