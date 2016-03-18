ebac3e8bcbfa09dd671291ae6ed995b5359bf9f6
niko <njichev@gmail.com>
2016-03-18 14:08:43 +0200

    Work on controllers.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,42 +1,40 @@
 class ItemsController < ApplicationController
-	def index
-		@items = Item.all
-	end
+  def index
+    @items = Item.all
+  end
 
-	def new
-		#only admin
-		@item = Item.new
-	end
+  def new
+    #only admin
+    @item = Item.new
+  end
 
-	def create
-		#only admin
-		#current_user.admin?
-		@item = Item.new(item_params)
+  def create
+    @item = Item.new(item_params)
     if @item.save
       redirect_to item_path(@item.id)
     else
       flash[:bad_input] = "Wrong input."
       redirect_to new_item_path
     end
-	end
+  end
 
-	def show
-		@item = Item.find(params[:id])
-	end
+  def show
+    @item = Item.find(params[:id])
+  end
 
-	def edit
-		#only admin
-	end
+  def edit
+    #only admin
+  end
 
-	def update
-		#only admin
-	end
+  def update
+    #only admin
+  end
 
-	def destroy
-		#only admin
-	end
+  def destroy
+    #only admin
+  end
 
-	private
+  private
 
   def item_params
     params.require(:item)
